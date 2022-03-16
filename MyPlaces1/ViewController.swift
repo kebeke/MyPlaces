1ea30f738b47f9592f8ffd7c6ff236ef9a521a1c
Angelina <kebeke@list.ru>
2022-03-17 00:07:09 +0100

    Initial Commit

---
+++ MyPlaces1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces1
+//
+//  Created by Johnny Bravo on 17/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
