b4dd86e2f85dda44196ca03be987c01e8b89b154
Angelina <kebeke@list.ru>
2022-03-17 18:16:57 +0100

    App Icon

--- MyPlaces1/MainViewController.swift
+++ MyPlaces1/MainViewController.swift
@@ -13,7 +13,7 @@ class MainViewController: UITableViewController {
         "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
         "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
         "Speak Easy", "Morris Pub", "Вкусные истории",
-        "Клаcсик", "Love&Life", "Шок", "Бочка"
+        "Классик", "Love&Life", "Шок", "Бочка"
     ]
 
     override func viewDidLoad() {
@@ -33,10 +33,18 @@ class MainViewController: UITableViewController {
 
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
     }
 
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+    
     /*
     // MARK: - Navigation
 
