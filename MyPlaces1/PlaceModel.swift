354353b63935222eabd30404299f743eef8b4c0c
Angelina <kebeke@list.ru>
2022-03-26 19:30:14 +0100

    Commit 4

---
+++ MyPlaces1/PlaceModel.swift
@@ -0,0 +1,34 @@
+//
+//  PlaceModel.swift
+//  MyPlaces1
+//
+//  Created by Johnny Bravo on 20/03/2022.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+}
